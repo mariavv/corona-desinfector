0f0904a8f319d7550e885d05236f8462b420aaeb
maria <mv.vasilyeva@mediasoft.team>
2021-10-11 17:22:25 +0400

    вторая имплементация сервиса, JavaConfig принимает мапу имплементаций и сохраняет нужную имплементацию

---
+++ src/main/java/org/AngryPoliceman.java
@@ -0,0 +1,8 @@
+package org;
+
+public class AngryPoliceman implements Policeman {
+    @Override
+    public void makePeopleLeaveRoom() {
+        System.out.println("Всех убью! Вон пошли");
+    }
+}
--- src/main/java/org/ObjectFactory.java
+++ src/main/java/org/ObjectFactory.java
@@ -3,16 +3,19 @@ package org;
 import lombok.SneakyThrows;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
-    private Config config = new JavaConfig("org");
+    private Config config;
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
 
     private ObjectFactory() {
+        config = new JavaConfig("org", new HashMap<>(Map.of(Policeman.class, AngryPoliceman.class)));
     }
 
     @SneakyThrows
